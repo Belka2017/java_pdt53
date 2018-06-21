f77f409f9f181e38b8f2f58719eb64e173340286
Ella <villo4k@mail.ru>
2018-06-21 23:31:48 +0300

    Les5_создан класс ApplicationManager, в который перенесены все вспомог.методы (шаг1 Замена наследия на делегирование)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class AddContactTests extends TestBase {
+
+    @Test
+    public void testAddContact() {
+        app.gotoAddNewContact();
+        app.fillNewContactForm(new ContactData("Ella", "Gololobova", "Moscow", "84956262626", "89052322323", "kot@mail.ru", "pol@mail.ru"));
+        app.submitNewContact();
+        app.returnToHomePage();
+    }
+
+}
