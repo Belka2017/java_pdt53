1b38d87828adad260d4b87580f683f9568f3f6e7
Ella <villo4k@mail.ru>
2018-07-08 12:50:38 +0300

    Les8_Добавлено предусловие в тест Удаление контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContact.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class DeleteContact extends TestBase {
 
     @Test
     public void testDeleteContact(){
         app.getContactHelper().initHome();
+        if (! app.getContactHelper().isThereAContact()){
+            app.getContactHelper().createContact(new ContactData("Ella", "Gololobova", "Moscow", "84956262626", "89052322323", "kot@mail.ru", "pol@mail.ru", "test1"));
+        }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
         app.getContactHelper().aceptDelete();
